1b799d7023f028b990f26a2c0dced58e76b329b8
SebastianCCC <U2407190@rts-365.dk>
2021-08-17 15:08:05 +0200

    add assinment 01c

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,4 +1,30 @@
+
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+const insertnumber = document.querySelector("#text-count");
+document.querySelector("#btn-count-increase").addEventListener("click", increase)
+document.querySelector("#btn-count-decrease").addEventListener("click", decrease)
+const numberincrease = 10;
+const numberdecrease = 0;
+insertnumber.textContent = 6;
+function increase() {
+		insertnumber.textContent = Number(insertnumber.textContent) + 1;
+		if(insertnumber.textContent > numberincrease){
+			console.log(`You are above ${numberincrease}.`)
+		}
+}
+function decrease(){
+		insertnumber.textContent = Number(insertnumber.textContent) - 1;
+		if(insertnumber.textContent < numberdecrease){
+			console.log(`You are below ${numberdecrease}.`)
+		}
+}
+/* if(insertnumber.textContent <= 0){
+	document.querySelector("#btn-count-increase").setAttribute("disabled", true);
+	document.querySelector("#btn-count-decrease").setAttribute("disabled", true);
+}else {
+	document.querySelector("#btn-count-increase").removeAttribute("disabled");
+	document.querySelector("#btn-count-decrease").removeAttribute("disabled");
+} */
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
