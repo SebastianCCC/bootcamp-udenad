e7c3f347c5842e372aefd1e2f47a0e0a9d88e275
SebastianCCC <U2407190@rts-365.dk>
2021-08-17 14:21:46 +0200

    add assinment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,4 +1,28 @@
+
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+const insertnumber = document.querySelector("#text-count");
+document.querySelector("#btn-count-increase").addEventListener("click", increase)
+document.querySelector("#btn-count-decrease").addEventListener("click", decrease)
+const numberincrease = 10;
+const numberdecrease = 0;
+insertnumber.textContent = 6;
+function increase() {
+	if(insertnumber.textContent < numberincrease){
+		insertnumber.textContent = Number(insertnumber.textContent) + 1;
+	}
+}
+function decrease(){
+	if(insertnumber.textContent > numberdecrease){
+		insertnumber.textContent = Number(insertnumber.textContent) - 1;
+	}
+}
+/* if(insertnumber.textContent <= 0){
+	document.querySelector("#btn-count-increase").setAttribute("disabled", true);
+	document.querySelector("#btn-count-decrease").setAttribute("disabled", true);
+}else {
+	document.querySelector("#btn-count-increase").removeAttribute("disabled");
+	document.querySelector("#btn-count-decrease").removeAttribute("disabled");
+} */
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
