db6a333decef12865a5631f67a52a9adeb76a647
SebastianCCC <U2407190@rts-365.dk>
2021-08-19 09:48:04 +0200

    add assinment 01c v2

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,23 +1,29 @@
 
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 const insertnumber = document.querySelector("#text-count");
-document.querySelector("#btn-count-increase").addEventListener("click", increase)
-document.querySelector("#btn-count-decrease").addEventListener("click", decrease)
+const change = document.querySelectorAll("button");
+/* document.querySelector("#btn-count-increase").addEventListener("click", increase)
+document.querySelector("#btn-count-decrease").addEventListener("click", decrease) */
 const numberincrease = 10;
 const numberdecrease = 0;
 insertnumber.textContent = 6;
-function increase() {
-		insertnumber.textContent = Number(insertnumber.textContent) + 1;
-		if(insertnumber.textContent > numberincrease){
-			console.log(`You are above ${numberincrease}.`)
+for(let i=0; i < 2; i++){
+    change[i].addEventListener("click", changenumber);
+   }
+
+   function changenumber(){
+    console.log(this.getAttribute("id"));
+
+    if(this.getAttribute("id")==="btn-count-increase"){
+        if(insertnumber.textContent < numberincrease){
+			insertnumber.textContent = Number(insertnumber.textContent) + 1;
 		}
-}
-function decrease(){
-		insertnumber.textContent = Number(insertnumber.textContent) - 1;
-		if(insertnumber.textContent < numberdecrease){
-			console.log(`You are below ${numberdecrease}.`)
-		}
-}
+            }else{
+				if(insertnumber.textContent > numberdecrease){
+					insertnumber.textContent = Number(insertnumber.textContent) - 1;
+				}
+        }
+   }
 /* if(insertnumber.textContent <= 0){
 	document.querySelector("#btn-count-increase").setAttribute("disabled", true);
 	document.querySelector("#btn-count-decrease").setAttribute("disabled", true);
