1e4a0e8cd53092048ae98a410fecd57bae434453
SebastianCCC <U2407190@rts-365.dk>
2021-08-20 09:33:33 +0200

    add assigment 03b

--- eks_03b_objekter/js/index.js
+++ eks_03b_objekter/js/index.js
@@ -1,4 +1,14 @@
-// "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
+const names = ["Batman", "Robin", "Raven", "Beastboy", "Starfire", "Cyborg"];
+const namesnew = ["Spiderman", "Ironman", "Captan Amarica", "Dr. Strange", "Black Panther", "Thor"];
+const licontent = document.querySelector("#liste");
+function alistofnames(array){
+	array.forEach((names) => { 
+		licontent.innerHTML += `<li>${names}</li>`;
+	}
+	)
+}
+alistofnames(names);
+alistofnames(namesnew);
 document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
